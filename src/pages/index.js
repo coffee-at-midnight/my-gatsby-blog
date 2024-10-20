f1d7ff1b03685e196b1b560cb538c5189134b8ba
Jana <noneofyourbusiness@yall.com>
2024-10-20 12:19:52 +0200

    Added image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
       <Layout pageTitle="Home Page">
       <p>This is not much, but it's better than nothing.</p>
+      <StaticImage
+        alt="Notebook with laptop and cup of coffee"
+        src="../images/writing-image.jpg"
+      />
       </Layout>
   )
 }
